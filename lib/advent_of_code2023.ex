8191af9affd02146f0a74615ecb8dd613ae6ec3a
christoph_weckert <christoph.weckert@unitybox.de>
2023-11-19 23:06:34 +0100

    Prepare Day1

--- lib/advent_of_code2023.ex
+++ lib/advent_of_code2023.ex
@@ -13,6 +13,7 @@ defmodule AdventOfCode2023 do
 
   """
   def hello do
+    IO.puts('Hello')
     :world
   end
 end
---
+++ lib/day1.ex
@@ -0,0 +1,9 @@
+# lib/Day1.ex
+
+# Call using mix run -e "AdventOfCode2023.Day1.solve"
+
+defmodule AdventOfCode2023.Day1 do
+  def solve do
+    IO.puts "Hello from Day 1!"
+  end
+end
